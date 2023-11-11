a37ec913a92f29c1c11d8b0ba9ab8f53bd6b45cb
Barsukov D <b.dmitry@alef.im>
2023-11-11 23:52:58 +0400

    Add TypeORM module and configuration to AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { getConfigDB } from './configs/db.config';
+
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getConfigDB,
+		}),
+		AuthModule,
+	],
 })
 export class AppModule {}
