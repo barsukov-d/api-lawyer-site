595bc30018c9bd94c6a962161d1599ebe94e2009
Barsukov D <b.dmitry@alef.im>
2023-11-12 09:32:49 +0400

    Update MySQL port configuration.

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -4,8 +4,8 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const getConfigDB = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
 	return {
 		type: 'mysql',
-		host: 'localhost',
-		port: 3306,
+		host: configService.get('HOST'),
+		port: configService.get('MYSQL_PORT'),
 		username: configService.get('MYSQL_USER'),
 		password: configService.get('MYSQL_PASSWORD'),
 		database: configService.get('MYSQL_DATABASE'),
