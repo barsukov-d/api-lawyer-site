88430c7af67aca6a454b843658682b1dec6fb6e2
Barsukov D <b.dmitry@alef.im>
2024-01-30 19:57:39 +0400

    Update workflow and database configuration

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -12,13 +12,13 @@ TODO: 'fix this function to return the config Promise<any>';
 export const getConfigDB = async (configService: ConfigService): Promise<any> => {
 	return {
 		dialect: 'mysql',
-		host: configService.get('HOST'),
+		host: configService.get('MYSQL_HOST'),
 		port: configService.get('MYSQL_PORT'),
 		username: configService.get('MYSQL_USER'),
 		password: configService.get('MYSQL_PASSWORD'),
 		database: configService.get('MYSQL_DATABASE'),
 		synchronize: true,
-		models: [User, Post, Page, Category, Tag, PostTag, FileInfo],
+		models: [User, Category, Post, Tag, Page, PostTag, FileInfo],
 		logging: console.log,
 	};
 };
